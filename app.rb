4d466d003d21b9b15cd150345c900116d3ddff00
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-11 21:40:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @text = TextAnalyzer.new(params[:user_text])
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
